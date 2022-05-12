0b4e75e95a78603d809be642a5007594f1cc2679
Victor <victor.anweiler@epitech.eu>
2022-05-12 22:48:48 +0200

    feature: redesign admin

--- resources/assets/js/admin/Views/Template.vue
+++ resources/assets/js/admin/Views/Template.vue
@@ -1,18 +1,6 @@
 <template>
     <div class="mb-5">
-        <!-- <div class="bg-white border my-rounded bg-light p-4">
-            <h5 class="mb-1">Administrateur</h5>
-            <h6 class="font-weight-light text-secondary mb-3">Documentation</h6>
-            <router-link :to="{ name: 'structure'}" class="d-block my-1 p-3 px-4 text-dark my-rounded" :class="btnIsSelected('structure')">
-                <i class="fas fa-sitemap" style="margin-right: 8px;"></i>Structure
-            </router-link>
-            <router-link :to="{ name: 'members'}" class="d-block my-1 p-3 px-4 text-dark my-rounded" :class="btnIsSelected('members')">
-                <i class="fas fa-users" style="margin-right: 8px;"></i>Membres
-            </router-link>
-        </div> -->
-        <div class="p-3  border my-rounded" style="min-height: 500px;">
-            <slot></slot>
-        </div>
+        <slot></slot>
     </div>
 </template>
 
