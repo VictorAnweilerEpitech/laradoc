84799d0da6e5e95362058680173f031d50313978
Victor <victor.anweiler@epitech.eu>
2022-05-13 21:30:18 +0200

    fix: use other dropdown in editor

--- resources/assets/js/admin/Components/Editor/TipTapButtons.vue
+++ resources/assets/js/admin/Components/Editor/TipTapButtons.vue
@@ -1,5 +1,36 @@
 <template>
     <div @mouseover="onOver" @mouseleave="onLeave">
+        <dropdown-menu
+        :dropup="true"
+        mode="hover"
+        >
+            <button slot="trigger" class="bg-white p-0 border-0">
+                <span
+                class="tiptap-btn rounded p-2 text-dark border"
+                >
+                    <template v-if="buttons.icon.split(' ')[0] == 'fas'">
+                        <i :class="buttons.icon"></i>
+                    </template>
+                    <template v-else-if="buttons.icon.split(' ')[0] == 'bi'">
+                        <b-icon :icon="buttons.icon.split(' ')[1]" />
+                    </template>
+                    <template v-else>
+                        <span v-html="buttons.icon"></span>
+                    </template>
+                </span>
+            </button>
+            <div slot="body" class="pb-3">
+                <tip-tap-button
+                v-for="(btn, index) in buttons.items"
+                :key="'index-' + buttons.icon + '-' + index"
+                :class="btn.class"
+                :icon="btn.icon"
+                :action="btn.action"
+                :is-active="btn.isActive"
+                />
+            </div>
+            <div slot="footer"></div>
+        </dropdown-menu>
         <!-- <b-dropdown variant="white" class="dropdown-button-tiptap" text="Dropdown Button" ref="dropdown">
             <template #button-content>
                 <span
--- resources/assets/js/admin/appadmin.js
+++ resources/assets/js/admin/appadmin.js
@@ -18,6 +18,13 @@ import "vue-toastification/dist/index.css";
 import Multiselect from 'vue-multiselect'
 Vue.component('multiselect', Multiselect)
 
+// Dropdown
+import Vue from 'vue'
+import DropdownMenu from 'v-dropdown-menu'
+import 'v-dropdown-menu/dist/v-dropdown-menu.css' // Base style, required.
+
+Vue.use(DropdownMenu)  
+
 const options = {};
 Vue.use(Toast, options);
 
